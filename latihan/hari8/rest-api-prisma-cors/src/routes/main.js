c6dd4d07bdc24d3325c6a27dfca8776997aea055
pribadita <pribadi.triagil@gmail.com>
2025-10-16 15:54:27 +0800

    validation middleware

--- latihan/hari8/rest-api-prisma-cors/src/routes/main.js
+++ latihan/hari8/rest-api-prisma-cors/src/routes/main.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const { createMovie, readMovie, readMovieById, updateMovie, deleteMovie} = require('../controllers/movieController')
+const { basicAuth } = require('../middleware/basicAuth')
+const { validationBodyMovies } = require('../middleware/validation')
 
 const router = express.Router()
 
 
-router.post('/movie', createMovie)
+router.post('/movie', basicAuth, validationBodyMovies, createMovie)
 router.get('/movie', readMovie)
 router.get('/movie/:id', readMovieById)
-router.put('/movie/:id', updateMovie)
-router.delete('/movie/:id', deleteMovie)
+router.put('/movie/:id', basicAuth, updateMovie)
+router.delete('/movie/:id', basicAuth, deleteMovie)
 
 
 
